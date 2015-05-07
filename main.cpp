279497bf63612ec8d23afe8ada98698b68e0e4fc
Max72590 <max-725@hotmail.com>
2015-05-07 11:51:33 -0700

    Main updated
    He añadido la lectura de la entrada y una funcion para printar el grafo, para debugar.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include "graph.cpp"
 
+
+void printgraph(graph& A){
+	int n = A.numfilas();
+	int m = A.numcolumnas();
+	cout<<"------------------------------"<<endl;
+	for (int i = 0; i < n; ++i){
+		for (int j = 0; j < m; ++j){
+			cout<<A.weight(i,j)<<" ";
+		}	
+		cout<<endl;
+	}
+	cout<<"------------------------------"<<endl;
+}
+
+
 int main() {
-	cout << "wea" << endl;
+	int n,m;
+	cin>> n >> m;
+	graph A = *new graph(n,m);
+	for (int i = 0; i < n; ++i){
+		for (int j = 0; j < m; ++j){
+			int value;
+			cin>>value;
+			A.addEdge(i,j,value);
+		}	
+	}
+	
+	printgraph(A);
 }
