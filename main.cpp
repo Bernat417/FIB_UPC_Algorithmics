16d81da3bc56961935c583253cad84d625781479
Max72590 <max-725@hotmail.com>
2015-05-26 12:38:04 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,16 @@
 int main() {
 	int n, m;
 	cin >> n >> m;
-	Graph G = *new Graph(n);
+	Graph G = *new Graph(n,m);
 	G.readGraph(m);
 	G.printgraph();
+	Algorithm A = *new Algorithm();
+	float f = 0.0;
+	Matrix Res = A.edmonskarp(G,f);
+	for (int i = 0; i < Res.size(); ++i){
+		for (int j = 0; j < Res.size(); ++j){
+			cout<<Res[i][j]<<" ";
+		}
+		cout<<endl;
+	}
 }
-
