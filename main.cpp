89f48eb362739039727aab2419db3ab878ce3d11
Drixide77 <adria.abella.93@gmail.com>
2015-06-07 21:50:04 +0200

    main.cpp cleaned

--- main.cpp
+++ main.cpp
@@ -1,79 +1,56 @@
 #include "algorithm.cpp"
 
-int main (int argc, char *argv[]) {
-
-	ofstream file;
-	file.open ("Resultados.txt");
-
-	int n, m;
-	cin >> n >> m;
-	Graph G = *new Graph(n,m);
-	bool tipoA = G.readGraph(m);
-	G.printgraph();
-	Algorithm A = *new Algorithm();
-	float f = 0.0;
-	Matrix Res;
+void printSol (const int& n, const int& m, const Matrix& Res) {
 	vector<int> sol(m);
-	cout << tipoA << endl;
-	//tipoA = false;
-	if (tipoA) {
-		Res = A.edmonskarp(G,f);
-		G.printgraph();
-		cout << f << " " << m << endl;
-		if (f == m) {
-			file << "A" << endl;
-			//file<<f<<endl;
-			for (int i = 2; i < n+2; ++i){
-				for (int j = n+2; j < m+n+2; ++j){
-					if (Res[i][j] == 1) sol[j-(n+2)] = i;
-				}
-			}
-			file << sol[0]-2;
-			for (int i = 1; i < sol.size(); ++i) file << " " << sol[i]-2;
-			file<<endl;
-			file.close();
-			return 0;
-		}
-	}
-	G.UpdateGraph1(m);
-	G.printgraph();
-	f = 0.0;
-	Res = A.edmonskarp(G,f);
-	cout << f << " " << m << endl;
-	if (f == m) {
-		file << "B" << endl;
-		//file<<f<<endl;
 		for (int i = 2; i < n+2; ++i){
 			for (int j = n+2; j < m+n+2; ++j){
 				if (Res[i][j] == 1) sol[j-(n+2)] = i;
 			}
 		}
-		file << sol[0]-2;
-			for (int i = 1; i < sol.size(); ++i) file << " " << sol[i]-2;
-		file<<endl;
-		file.close();
+	cout << sol[0]-2;
+	for (int i = 1; i < sol.size(); ++i) cout << " " << sol[i]-2;
+	cout << endl;
+}
+
+int main (int argc, char *argv[]) {
+	int n, m;
+	cin >> n >> m;
+	Graph G = *new Graph(n,m);
+	bool tipoA = G.readGraph(m);
+	//G.printgraph();
+	Algorithm A = *new Algorithm();
+	float f = 0.0;
+	Matrix Res;
+	if (tipoA) {
+		Res = A.edmonskarp(G,f);
+		//G.printgraph();
+		if (f == m) {
+			cout << "A" << endl;
+			printSol(n, m, Res);
+			return 0;
+		}
+	}
+	G.UpdateGraph1(m);
+	//G.printgraph();
+	f = 0.0;
+	Res = A.edmonskarp(G,f);
+	if (f == m) {
+		cout << "B" << endl;
+		printSol(n, m, Res);
 		return 0;
 	}
 	else {
 		G.UpdateGraph2(m);
-		G.printgraph();
+		//G.printgraph();
 		f = 0.0;
 		Res = A.edmonskarp(G,f);
 		if (f == m){
-			file<<"C"<<endl;
-			for (int i = 2; i < n+2; ++i){
-				for (int j = n+2; j < m+n+2; ++j){
-					if (Res[i][j] == 1) sol[j-(n+2)] = i;
-				}
-			}
-			file << sol[0]-2;
-			for (int i = 1; i < sol.size(); ++i) file << " " << sol[i]-2;
-			file<<endl;
+			cout << "C" << endl;
+			printSol(n, m, Res);
 		}
 		else {
-			file<<"D"<<endl;
+			cout << "D" << endl;
 		}
 	}
-	file.close();
 	return 0;
 }
