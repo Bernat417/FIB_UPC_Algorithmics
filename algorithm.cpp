79c84b77e10ae017af9fa6502824965f5aae5cbc
Bernat417 <bernat.huguet@gmail.com>
2015-06-08 20:32:40 +0200

    test

--- algorithm.cpp
+++ algorithm.cpp
@@ -4,11 +4,6 @@
 class Algorithm {
 
 public:
-
-	Algorithm(){
-
-	}
-	
 	//--------------- EDMONDS-KARP ----------------//
 	/*BFS*/
 
@@ -73,6 +68,9 @@ public:
 //https://sites.google.com/site/indy256/algo/preflow
 //http://cophy-wiki.informatik.uni-koeln.de/index.php/The_push-relabel_algorithm
 
+
+
+
 	Matrix preflow(Graph& A,float& f){
 		int n = A.getNumPersonas();
 		vector<float> height(n,0);		// Or "label distance".
@@ -81,51 +79,73 @@ public:
 		height[0] = n;
 		Matrix F(A.size(), vector<float>(A.size(),0));
 		vector<WeightedEdge> aux = A.getNeighbours(0);
-		for (int i = 0; i < aux.size(); ++i){
+
+		for (int i = 0; i < aux.size(); ++i)
+		{
 			float w = A.weight(0,i); 
 			F[0][i] = w;
 			F[i][0] = -w;
-			exceso[i] = w;
+			exceso[0] = w;
 		}
+
 		for (int a = 0; a< n; ++a){  // Hasta n? no estoy muy seguro.
-			if (a == 0){
-				for (int i = 0; i < n; ++i){
+			if (a == 0)
+				for (int i = 1; i < n; ++i)
+				{
 					if ((i !=0) and (i != A.size()-1) and exceso[i]>0){
-						if (a != 0 and height[i] > height[maxheight[0]]) a = 0;
-						maxheight[++a] = i;						
+						if ((a != 0) and (height[i] > height[maxheight[0]])) a = 0;
+						maxheight[++a] = i;	
+									
 					}
 				}
-			}
+
 			if (a == 0)	break;
-			while (a != 0){
+			cout << "IN" << endl;
+			cout << a << endl;
+			while (a != 0)
+			{
 				int i = maxheight[a-1];
 				bool push = false;
-				for (int j = 0; j < n && exceso[i]!=0; ++j){
-					if (height[i]==height[j] +1 and A.weight(i,j)- F[i][j] > 0){
+				for (int j = 0;(j < n) && (exceso[i]!=0); ++j)
+				{
+					if ((height[i]==height[j] +1 ) and (A.weight(i,j)- F[i][j] > 0))
+					{
+						cout << "INSIDE" << endl;
 						int resto = min(A.weight(i,j)- F[i][j],exceso[i]);
 						F[i][j] += resto;
-						F[j][i] = resto;
+						F[j][i] -= resto;
 						exceso[i] -= resto;
 						exceso[j] += resto;
 						if (exceso[i] == 0) --a;
 						push = true;
 					}
 				}
+				if(push) cout << "YOLO" <<endl;
 				if (not push){
 					height[i] = INFINITY;
-					for (int z =0; z < n; ++z){
-						if (height[i] > height[z] +1 and A.weight(i,z)- F[i][z] > 0){
+					for (int z =0; z < n; ++z)
+					{
+						if(A.weight(i,z)- F[i][z] > 0) cout << "CHANGE" << endl;
+						if ((height[i] > height[z] +1) and (A.weight(i,z)- F[i][z] > 0)){
 							height[i] = height[z] +1;
+
 						}
 					}
-					if (height[i] > height[maxheight[0]]){
+					if (height[i] > height[maxheight[0]])
+					{
 						a = 0;
+						cout << "out"<< endl;	
 						break;
+
 					}
+					 cout <<  height[maxheight[0]] << endl;
 				}
+				else cout << "HERE" << endl;
 			}
+			cout << "OUT" << endl;
 		}
-		for (int i = 0; i < n; ++i){
+		for (int i = 0; i < n; ++i)
+		{
 			f += F[0][i]; 
 		}
 		return F;
